3767b110fc84fdd67eb5144dd0b1616a879c042f
angelica1993 <folkesson.angelica@gmail.com>
2023-06-20 08:45:38 +0200

    la till lite css och hämtade element till JS

--- main.js
+++ main.js
@@ -1,1 +1,5 @@
-
+// Hämtar element från HTML och skapar en array för alla Todos
+let todoContainer = document.getElementById("todo-list"); 
+let inputTitle = document.getElementById("title-input"); 
+let inputDescription = document.getElementById("description-input"); 
+let allTodos = []; 
