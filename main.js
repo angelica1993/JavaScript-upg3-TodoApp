8332a540aac855e7e593cbfa3e4ff99cb1c31cf5
angelica1993 <folkesson.angelica@gmail.com>
2023-06-20 09:01:47 +0200

    la till mer funktioner

--- main.js
+++ main.js
@@ -46,3 +46,54 @@ function renderTodos() {
     todoContainer.appendChild(todoItem) 
   }) 
 } 
+
+// Funktion för att skapa / styla / visa upp todos
+function createTodoItem(todo, index) { 
+  let todoItem = document.createElement("li"); 
+  if (todo.completed) { 
+    todoItem.classList.add("completed"); 
+  } 
+
+  let completedBtn = document.createElement("input"); 
+  completedBtn.type = "checkbox"; 
+  completedBtn.checked = todo.completed; 
+  completedBtn.onchange = () => completedTodos(index); 
+
+  let titleInput = document.createElement("span"); 
+  titleInput.textContent = todo.title; 
+  titleInput.style = "font-weight: bold; margin-right: 15px; margin-left: 15px; font-size: 20px;"; 
+
+  let descriptionInput = document.createElement("span"); 
+  descriptionInput.textContent = todo.description; 
+  descriptionInput.style = "font-size: 16px; margin-right: 30px;" 
+
+  let createdDateStamp = document.createElement("span"); 
+  createdDateStamp.textContent = `Created: ${todo.createdDate.toLocaleString()}`; 
+  createdDateStamp.style = "font-style: italic; color: gray; margin-right: 15px;" 
+
+  let completedDateStamp = document.createElement("span"); 
+  completedDateStamp.style = "font-style: italic; color: gray;" 
+  
+  if(todo.completed) { 
+  completedDateStamp.textContent = `Completed: ${todo.completedDate.toLocaleString()}`; 
+  } 
+
+  let removeBtn = document.createElement("button"); 
+  removeBtn.textContent = "Remove"; 
+  removeBtn.onclick = () => removeTodo(index); 
+
+  todoItem.appendChild(completedBtn); 
+  todoItem.appendChild(titleInput); 
+  todoItem.appendChild(descriptionInput); 
+  todoItem.appendChild(createdDateStamp); 
+  todoItem.appendChild(completedDateStamp); 
+  todoItem.appendChild(removeBtn); 
+
+  return todoItem; 
+} 
+
+// Funktion för att rensa inmatningsfälten
+function clearInputs() { 
+  inputTitle.value = ""; 
+  inputDescription.value = ""; 
+} 
